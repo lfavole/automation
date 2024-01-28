acaa57b1c94b53d4c5b4cf3c90b26ca15258fbeb
lfavole <88188568+lfavole@users.noreply.github.com>
2024-01-28 09:36:04 +0100

    Add Todoist app

--- get_refresh_token_app.py
+++ get_refresh_token_app.py
@@ -1,9 +1,8 @@
 from urllib.parse import unquote, urlencode
 
-from flask import Flask, redirect, request, session
 import custom_requests
+from flask import Flask, abort, redirect, request, session
 from get_secrets import get_secret
-
 from oauth_token import Token
 
 app = Flask(__name__)
@@ -14,8 +13,14 @@ def index():
     return """\
 <ul>
     <li><a href="/google">Google</a></li>
+    <li><a href="/todoist">Todoist</a></li>
 </ul>
 """
+
+
+@app.route("/favicon.ico")
+def favicon():
+    abort(404)
 
 
 @app.route("/<provider>")
@@ -39,12 +44,27 @@ def provider_oauth(provider):
     if "code" in params:
         access_token_method = {
             "google": "POST",
+            "todoist": "POST",
         }[provider]
         access_token_url = {
             "google": "https://oauth2.googleapis.com/token",
+            "todoist": "https://todoist.com/oauth/access_token",
         }[provider]
 
+        if provider == "todoist":
+            state = session["state"]
+            if state != params["state"]:
+                return "State mismatch"
+
         code = params["code"]
+        get_access_token_data = {
+            "google": {
+                "grant_type": "authorization_code",
+                "redirect_uri": redirect_uri,
+            },
+            "todoist": {},
+        }
+
         req = custom_requests.request(
             access_token_method,
             access_token_url,
@@ -52,8 +72,7 @@ def provider_oauth(provider):
                 "client_id": client_id,
                 "client_secret": client_secret,
                 "code": code,
-                "grant_type": "authorization_code",
-                "redirect_uri": redirect_uri,
+                **get_access_token_data,
             },
         )
         data = req.json()
@@ -65,20 +84,21 @@ def provider_oauth(provider):
 
     url = {
         "google": "https://accounts.google.com/o/oauth2/v2/auth",
+        "todoist": "https://todoist.com/oauth/authorize",
     }[provider]
     params = {
         "google": {
-            "scope": " ".join(
-                [
-                    "https://www.googleapis.com/auth/tasks",
-                    "https://www.googleapis.com/auth/gmail.readonly",
-                ]
-            ),
+            "scope": "https://www.googleapis.com/auth/tasks https://www.googleapis.com/auth/gmail.readonly",
             "access_type": "offline",
             "response_type": "code",
             "redirect_uri": redirect_uri,
             "client_id": client_id,
             "include_granted_scopes": "true",
+        },
+        "todoist": {
+            "client_id": client_id,
+            "scope": "task:add data:delete",
+            "state": session.setdefault("TODOIST_STATE", str(uuid.uuid4())),
         },
     }[provider]
 
