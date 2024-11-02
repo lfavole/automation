37d7ae8a91024968d20c1c759530c6253e14eb6a
Laurent FAVOLE <laurentfavole03@gmail.com>
2024-11-02 11:59:53 +0100

    fix: all environment variables are saved to .env

--- get_secrets.py
+++ get_secrets.py
@@ -7,21 +7,50 @@ NOT_PROVIDED = object()
 
 
 class Secrets(dict):
+    """A dict that returns secrets from the `.env` file or from environment variables."""
+
     def __init__(self):
+        super().__init__()
+
         self.file = Path(__file__).parent / ".env"
-        data = os.environ.copy()
         if self.file.exists():
             with self.file.open() as f:
                 for line in f:
                     key, _, value = line[:-1].partition("=")
-                    data[key] = value
-        super().__init__(data)
+                    self[key] = value
+
+    def __getitem__(self, key):
+        try:
+            return super().__getitem__(key)
+        except KeyError:
+            return os.environ[key]
+
+    def get(self, key, default):
+        if key not in self:
+            return os.environ.get(key, default)
+        return super().get(key, default)
+
+    def setdefault(self, key, default):
+        if key not in self and key in os.environ:
+            return os.environ[key]
+        return super().setdefault(key, default)
+
+    def pop(self, key):
+        ret = super().pop(key)
+        self.save()
+        return ret
+
+    def popitem(self):
+        ret = super().popitem()
+        self.save()
+        return ret
 
     def __setitem__(self, key, value):
         super().__setitem__(key, value)
         self.save()
 
     def save(self):
+        """Save the secrets to the `.env` file."""
         new_file = self.file.with_name(self.file.name + ".new")
         with new_file.open("w") as f:
             for key, value in self.items():
